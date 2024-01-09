dc77101e1b4018828eafd0ea950336f64c54b39b
Fabricio <fabriciopolicarpo0@gmail.com>
2024-01-09 18:40:53 -0500

    store split

--- src/components/Dialog/FormDialog.tsx
+++ src/components/Dialog/FormDialog.tsx
@@ -1,48 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// import { IconButton, useDisclosure } from "@chakra-ui/react";
-// import { FormDialog } from "@saas-ui/forms/zod";
-// import { FiSettings } from "react-icons/fi";
-
-//
-// import { getNodeFormConfiguration } from "../../utils/getFormConfiguration";
-// import { z } from "zod";
-
-// export function FormModal({ data }: { data: z.infer<z.ZodAny> }) {
-//   const onNodeDataChange = useStore.use.onNodeDataChange();
-//   const disclosure = useDisclosure();
-
-//   const formData = getNodeFormConfiguration(
-//     data.serviceInfo.provider,
-//     data.serviceInfo.type
-//   );
-
-//   return (
-//     <>
-//       <IconButton
-//         aria-label="Configuration"
-//         icon={<FiSettings />}
-//         onClick={() => disclosure.onOpen()}
-//       />
-
-//       <FormDialog
-//         title={`Configure your ${data.serviceInfo.type}`}
-//         schema={formData.schema}
-//         {...disclosure}
-//         defaultValues={{
-//           configuration: {
-//             tags: [],
-//           },
-//         }}
-//         fields={formData.fields}
-//         onSubmit={async (d) => {
-//           await onNodeDataChange(data, d);
-//           disclosure.onClose();
-//         }}
-//       />
-//     </>
-//   );
-// }
-
 import { IconButton, useDisclosure } from "@chakra-ui/react";
 import {
   FormDialog,
